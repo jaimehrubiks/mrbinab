5c53f8f1211ae2969b2c864a06f642d60d24e84c
Jaime Hidalgo García <jaimehidalgo2@hotmail.com>
2015-12-26 21:11:30 +0100

    Refactor
    Code refactored with status up to level 3

--- src/binab.py
+++ src/binab.py
@@ -1,8 +1,7 @@
 import asyncio
 import threading
-
 import telepot
-import time
+
 from telepot.delegate import per_chat_id
 from telepot.async.delegate import create_open
 
@@ -22,33 +21,32 @@ LICENSE:        BPL V3 in file "LICENSE"
 # Configuration
 TIMEOUT_SECONDS = 200
 
-
+MAX_STATUS = 3              # Min: 3 (Avoid initial conversation)
 # End configuration
 
 
 class User(telepot.helper.ChatHandler):
-
     def __init__(self, seed_tuple, timeout):
         super(User, self).__init__(seed_tuple, timeout)
-        self._connected = False
+        self._status = 0
         self._id = None
         self._peerid = None
 
     @asyncio.coroutine
     def open(self, initial_msg, seed):
         yield from self.sender.sendMessage('boot_message')
-        return True  # prevent on_message() from being called on the initial message
+        return False  # prevent on_message() from being called on the initial message
 
     @asyncio.coroutine
     def disconnect(self):
         yield from self.sender.sendMessage(u"forced_disconnect")
-        self._connected = False
+        self._status = 0
         return
 
     @asyncio.coroutine
     def start_peer(self, peerid):
         self._peerid = peerid
-        self._connected = True
+        self._status = 4
         yield from self.sender.sendMessage(u"connected_a2")
 
     @asyncio.coroutine
@@ -64,22 +62,23 @@ class User(telepot.helper.ChatHandler):
 
         message = msg['text']
 
-        if not self._connected:
-
-            if self._id is None: self._id = msg['from']['id']
-            if self._id not in user_list: user_list[self._id] = self
-
-            if message == "/init":
-                self._peerid = in_out_queue(self._id)
-                if self._peerid == 0:
-                    yield from self.sender.sendMessage('connected_a1')
+        if self._status < MAX_STATUS:
+            if self._status == 0:
+                self._id = msg['from']['id']
+                user_list[self._id] = self
+                self._status += 1
+            elif self._status == 1:
+                if message == '/init':
+                    self._peerid = in_out_queue(self._id)
+                    if self._peerid == 0:
+                        yield from self.sender.sendMessage('connected_a1')
+                        self._status += 1
+                    else:
+                        self._status += 2
+                        yield from self.sender.sendMessage('connected_b')
+                        yield from user_list.get(self._peerid).start_peer(self._id)
                 else:
-                    self._connected = True
-                    yield from self.sender.sendMessage('connected_b')
-                    yield from user_list.get(self._peerid).start_peer(self._id)
-
-            else:
-                yield from self.sender.sendMessage(u"Utilice /init o espere")
+                    yield from self.send_message('instructions')
             return
 
         if message == "/end":
